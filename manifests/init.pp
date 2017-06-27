71d40856bca4f2a18caa541689cf58d919255fac
Florian Klink <flokli@flokli.de>
2017-06-27 17:50:52 +0200

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class docker {
+  include apt
+
+  apt::key {'docker':
+    id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+    server => 'pool.sks-keyservers.net',
+  }
+
+  apt::source {'docker':
+    location => 'https://download.docker.com/linux/debian',
+    require  => Apt::Key['docker'],
+  }
+
+  package { 'docker-ce':
+    ensure  => present,
+    require => [Class['apt::update'], Apt::Source['docker']],
+  }
+}
